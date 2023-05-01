882fe7290cc37cbafe680cf3b95f52724fba3593
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2023-05-01 12:08:56 +0530

    Update Test.java

--- src/TestAutomation/Test.java
+++ src/TestAutomation/Test.java
@@ -1,184 +1,221 @@
 package TestAutomation;
 
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 class Testing {
-	
+
+	@Test
 	void home() {
-		
-		String driverpath = "E:\\UPES\\SEM 6\\Lab\\Test Automation\\fire\\geckodriver.exe";
-		System.setProperty("webdriver.gecko.driver", driverpath);
-		WebDriver driver = new FirefoxDriver();
+
+		System.setProperty("webdriver.chrome.driver", "C:\\fire\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
-		
-		driver.get("http://35.92.179.38:3000/");
-		
+
+		driver.get("http://52.89.7.185:3000/");
+
 		System.out.println("Home Page Testing:");
-		
+
 		WebElement title = driver.findElement(By.xpath("/html/body/div/div/header/div/span"));
-		boolean titlei = title.isDisplayed();
+		boolean titlei = title.isEnabled();
 		System.out.println("Title Displayed: " + titlei);
-		
+		AssertJUnit.assertEquals(titlei, true);
+
 		WebElement image = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/img"));
-		boolean imagei = image.isDisplayed();
-		System.out.println("Image Displayed: "+imagei);
+		boolean imagei = image.isEnabled();
+		System.out.println("Image Displayed: " + imagei);
+		AssertJUnit.assertEquals(imagei, true);
 
 		WebElement loginButton = driver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[1]/div[2]/a"));
-		boolean loginb =loginButton.isEnabled();
-		System.out.println("Login Button Clickable: "+loginb);
-		
+		boolean loginb = loginButton.isEnabled();
+		System.out.println("Login Button Clickable: " + loginb);
+		AssertJUnit.assertEquals(loginb, true);
+
 		WebElement registerButton = driver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[1]/div[1]/a"));
-		boolean registerb =registerButton.isEnabled();
-		System.out.println("Register Button Clickable: "+registerb);
-		
+		boolean registerb = registerButton.isEnabled();
+		System.out.println("Register Button Clickable: " + registerb);
+		AssertJUnit.assertEquals(registerb, true);
+
 		WebElement searchButton = driver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[2]/form/button"));
-		boolean searchb =searchButton.isEnabled();
-		System.out.println("Search Button Clickable: "+searchb);
-		
+		boolean searchb = searchButton.isEnabled();
+		System.out.println("Search Button Clickable: " + searchb);
+		AssertJUnit.assertEquals(searchb, true);
+
 		WebElement petIdBox = driver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[2]/form/input"));
-		boolean petIdboxb =petIdBox.isDisplayed();
-		System.out.println("Enter Pet Id Box Displayed: "+petIdboxb);
-	
+		boolean petIdboxb = petIdBox.isEnabled();
+		System.out.println("Enter Pet Id Box Displayed: " + petIdboxb);
+		AssertJUnit.assertEquals(petIdboxb, true);
+
 		driver.close();
-		
+
 	}
-	
+
+	@Test
 	void login() {
-		
-		String driverpath = "E:\\UPES\\SEM 6\\Lab\\Test Automation\\fire\\geckodriver.exe";
-		System.setProperty("webdriver.gecko.driver", driverpath);
-		WebDriver driver = new FirefoxDriver();
+
+		System.setProperty("webdriver.chrome.driver", "C:\\fire\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
-		
-		driver.get("http://35.92.179.38:3000/");
-		
+
+		driver.get("http://52.89.7.185:3000/login");
+
 		System.out.println("Login Page Testing:");
-		
-		WebElement loginButton = driver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[1]/div[2]/a"));
-		loginButton.click();
-		
+
 		WebElement emailBox = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/form/input[1]"));
-		boolean emailboxb =emailBox.isEnabled();
-		System.out.println("Email Box Displayed: "+emailboxb);
+		boolean emailboxb = emailBox.isEnabled();
+		System.out.println("Email Box Displayed: " + emailboxb);
 		emailBox.sendKeys("usertwo@gmail.com");
-		
+		AssertJUnit.assertEquals(emailboxb, true);
+
 		WebElement passBox = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/form/input[2]"));
-		boolean passboxb =passBox.isEnabled();
-		System.out.println("Password Box Displayed: "+passboxb);
+		boolean passboxb = passBox.isEnabled();
+		System.out.println("Password Box Displayed: " + passboxb);
 		passBox.sendKeys("12345678");
-		
+		AssertJUnit.assertEquals(passboxb, true);
+
 		WebElement loginbutton = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/form/button"));
-		boolean loginbuttonb =loginbutton.isEnabled();
-		System.out.println("Login Button Displayed: "+loginbuttonb);	
-		
+		boolean loginbuttonb = loginbutton.isEnabled();
+		System.out.println("Login Button Displayed: " + loginbuttonb);
+		AssertJUnit.assertEquals(loginbuttonb, true);
+
 		driver.close();
-		
+
 	}
-	
+
+	@Test
 	void register() {
-		
-		String driverpath = "E:\\UPES\\SEM 6\\Lab\\Test Automation\\fire\\geckodriver.exe";
-		System.setProperty("webdriver.gecko.driver", driverpath);
-		WebDriver driver = new FirefoxDriver();
+
+		System.setProperty("webdriver.chrome.driver", "C:\\fire\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
-		
-		driver.get("http://35.92.179.38:3000/");
-		
+
+		driver.get("http://52.89.7.185:3000/signup");
+
 		System.out.println("Register Page Testing:");
-		
-		WebElement registerButton = driver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[1]/div[1]/a"));
-		registerButton.click();
-		
+
 		WebElement nameBox = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/form/input[1]"));
-		boolean nameboxb =nameBox.isEnabled();
-		System.out.println("Name Box Displayed: "+nameboxb);
+		boolean nameboxb = nameBox.isEnabled();
+		System.out.println("Name Box Displayed: " + nameboxb);
 		nameBox.sendKeys("user Two");
-		
+		AssertJUnit.assertEquals(nameboxb, true);
+
 		WebElement emailBox = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/form/input[2]"));
-		boolean emailboxb =emailBox.isEnabled();
-		System.out.println("Email Box Displayed: "+emailboxb);
+		boolean emailboxb = emailBox.isEnabled();
+		System.out.println("Email Box Displayed: " + emailboxb);
 		emailBox.sendKeys("userthree@gmail.com");
-		
+		AssertJUnit.assertEquals(emailboxb, true);
+
 		WebElement passBox = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/form/input[3]"));
-		boolean passboxb =passBox.isEnabled();
-		System.out.println("Password Box Displayed: "+passboxb);
+		boolean passboxb = passBox.isEnabled();
+		System.out.println("Password Box Displayed: " + passboxb);
 		passBox.sendKeys("12345678");
-		
+		AssertJUnit.assertEquals(passboxb, true);
+
 		WebElement phoneBox = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/form/input[4]"));
-		boolean phoneboxb =phoneBox.isEnabled();
-		System.out.println("Phone No Box Displayed: "+phoneboxb);
+		boolean phoneboxb = phoneBox.isEnabled();
+		System.out.println("Phone No Box Displayed: " + phoneboxb);
 		phoneBox.sendKeys("1234567890");
-		
+		AssertJUnit.assertEquals(phoneboxb, true);
+
 		WebElement registerbutton = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/form/button"));
-		boolean registerbuttonb =registerbutton.isEnabled();
-		System.out.println("Register Button Displayed: "+registerbuttonb);	
+		boolean registerbuttonb = registerbutton.isEnabled();
+		System.out.println("Register Button Displayed: " + registerbuttonb);
 		registerbutton.click();
-		
+		AssertJUnit.assertEquals(registerbuttonb, true);
+
 		driver.close();
 	}
-	
+
+	@Test
 	void search() {
-		
-		String driverpath = "E:\\UPES\\SEM 6\\Lab\\Test Automation\\fire\\geckodriver.exe";
-		System.setProperty("webdriver.gecko.driver", driverpath);
-		WebDriver driver = new FirefoxDriver();
+
+		System.setProperty("webdriver.chrome.driver", "C:\\fire\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
-		
-		driver.get("http://35.92.179.38:3000/");
-		
+
+		driver.get("http://52.89.7.185:3000/");
+
 		System.out.println("Pet Details Page Testing:");
-		
+
 		WebElement petIdBox = driver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[2]/form/input"));
 		petIdBox.sendKeys("b6YJhHVk");
-		
+
 		WebElement searchButton = driver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[2]/form/button"));
 		searchButton.click();
-		
+
 		WebElement heading = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/h2"));
 		boolean headingb = heading.isEnabled();
-		System.out.println("Heading is Displayed: "+headingb);
-		
-		WebElement petid = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[1]/ul/li[1]/span"));
+		System.out.println("Heading is Displayed: " + headingb);
+		AssertJUnit.assertEquals(headingb, true);
+
+		WebElement petid = driver
+				.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[1]/ul/li[1]/span"));
 		boolean petidb = petid.isEnabled();
-		System.out.println("PetID is Displayed: "+petidb);
-		
-		WebElement petName = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[1]/ul/li[2]/span"));
+		System.out.println("PetID is Displayed: " + petidb);
+		AssertJUnit.assertEquals(petidb, true);
+
+		WebElement petName = driver
+				.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[1]/ul/li[2]/span"));
 		boolean petNameb = petName.isEnabled();
-		System.out.println("Pet Name is Displayed: "+petNameb);
-		
-		WebElement petCategory = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[1]/ul/li[3]/span"));
+		System.out.println("Pet Name is Displayed: " + petNameb);
+		AssertJUnit.assertEquals(petNameb, true);
+
+		WebElement petCategory = driver
+				.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[1]/ul/li[3]/span"));
 		boolean petCategoryb = petCategory.isEnabled();
-		System.out.println("Pet Category is Displayed: "+petCategoryb);
-		
-		WebElement petIdentifications = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[1]/ul/li[4]/span"));
+		System.out.println("Pet Category is Displayed: " + petCategoryb);
+		AssertJUnit.assertEquals(petCategoryb, true);
+
+		WebElement petIdentifications = driver
+				.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[1]/ul/li[4]/span"));
 		boolean petIdentificationsb = petIdentifications.isEnabled();
-		System.out.println("Pet Identifications is Displayed: "+petIdentificationsb);
-		
-		WebElement petBreed = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[2]/ul/li[1]/span"));
+		System.out.println("Pet Identifications is Displayed: " + petIdentificationsb);
+		AssertJUnit.assertEquals(petIdentificationsb, true);
+
+		WebElement petBreed = driver
+				.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[2]/ul/li[1]/span"));
 		boolean petBreedb = petBreed.isEnabled();
-		System.out.println("Pet Breed is Displayed: "+petBreedb);
-		
-		WebElement petGender = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[2]/ul/li[2]/span"));
+		System.out.println("Pet Breed is Displayed: " + petBreedb);
+		AssertJUnit.assertEquals(petBreedb, true);
+
+		WebElement petGender = driver
+				.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[2]/ul/li[2]/span"));
 		boolean petGenderb = petGender.isEnabled();
-		System.out.println("Pet Gender is Displayed: "+petGenderb);
-		
-		WebElement petColour = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[2]/ul/li[3]/span"));
+		System.out.println("Pet Gender is Displayed: " + petGenderb);
+		AssertJUnit.assertEquals(petGenderb, true);
+
+		WebElement petColour = driver
+				.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[2]/ul/li[3]/span"));
 		boolean petColourb = petColour.isEnabled();
-		System.out.println("Pet Colour is Displayed: "+petColourb);
-		
-		WebElement petOwner = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[2]/ul/li[4]/span"));
+		System.out.println("Pet Colour is Displayed: " + petColourb);
+		AssertJUnit.assertEquals(petColourb, true);
+
+		WebElement petOwner = driver
+				.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div[2]/ul/li[4]/span"));
 		boolean petOwnerb = petOwner.isEnabled();
-		System.out.println("Pet Owner is Displayed: "+petOwnerb);
-		
+		System.out.println("Pet Owner is Displayed: " + petOwnerb);
+		AssertJUnit.assertEquals(petOwnerb, true);
+
 		WebElement petButton = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/button"));
 		boolean petButtonb = petButton.isEnabled();
-		System.out.println("Contact Owner is Displayed and Enabled: "+petButtonb);
-		
+		System.out.println("Contact Owner is Displayed and Enabled: " + petButtonb);
+		AssertJUnit.assertEquals(petButtonb, true);
+
 		driver.close();
-		
+
 	}
 }
 
@@ -189,6 +226,8 @@ public class Test {
 		obj.home();
 		obj.login();
 		obj.register();
-		obj.search();	
+		obj.search();
+
 	}
+
 }
